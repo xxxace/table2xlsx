9697a05eb6f3f2c5581dbbf2fc7d7b1fdbd141ee
wym <731162160@qq.com>
2022-03-25 14:13:15 +0800

    feture: using table Elemnt to export

--- src/components/Table2Xlsx.ts
+++ src/components/Table2Xlsx.ts
@@ -5,16 +5,17 @@ import {IColumn, IData} from "../../types/index";
 
 export interface IOption {
     fileName: string
-    columns: IColumn[]
-    dataSource: IData[]
+    target?: Element
+    columns?: IColumn[]
+    dataSource?: IData[]
 }
 
 export function createTable(columns: IColumn[], dataSource: IData[]): HTMLTableElement {
     return Table.create(columns, dataSource);
 }
 
-export function getBytes(columns:IColumn[] = [],dataSource:IData[] = []) {
-    const table = createTable(columns, dataSource);
+export function getBytes(option: IOption) {
+    const table = option.target || createTable(option.columns || [], option.dataSource || []);
     const workbook = utils.table_to_book(table);
     const bytes = write(workbook, {bookType: 'xlsx', bookSST: true, type: 'array'});
     return bytes
@@ -22,7 +23,7 @@ export function getBytes(columns:IColumn[] = [],dataSource:IData[] = []) {
 
 export function getExcel(option: IOption) {
     const fileName = option.fileName + '.xlsx';
-    const bytes = getBytes(option.columns, option.dataSource)
+    const bytes = getBytes(option);
 
     try {
         const type = 'application/octet-stream';
@@ -34,7 +35,7 @@ export function getExcel(option: IOption) {
 
 export function getExcelSync(option: IOption) {
     const fileName = option.fileName + '.xlsx';
-    const bytes = getBytes(option.columns, option.dataSource)
+    const bytes = getBytes(option);
 
     return new Promise(async (resolve, reject) => {
         try {
